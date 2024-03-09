6faf59729a87890012dd5a946cf34103bf583694
oksanakney <oksanakney@gmail.com>
2024-03-09 20:27:45 +0200

    Task Become Agent created

--- HouseRentingSystem/Controllers/AgentController.cs
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HouseRentingSystem.Services.Data.Interfaces;
+using HouseRentingSystem.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HouseRentingSystem.Web.Controllers
 {
+    using static Common.NotificationMessagesConstants;
+
     [Authorize]
     public class AgentController : Controller
     {
+        private readonly IAgentService agentService;
+        public AgentController(IAgentService agentService)
+        {
+            this.agentService = agentService;
+        }
         [HttpGet]
         public async Task<IActionResult> Become()
-        {            
-            return View();
+        {
+            string? userId = this.User.GetId();
+            bool isAgent = await this.agentService.AgentExistsByUserId(userId);
+            if (isAgent)
+            {
+                TempData[ErrorMessage] = "You are already an agent!";
+
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            return this.View();
         }
     }
 }
