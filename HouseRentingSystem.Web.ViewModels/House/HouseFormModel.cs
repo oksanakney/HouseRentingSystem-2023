2803d059b3c0bdd0d801263bc9372ee82b40cbfe
oksanakney <oksanakney@gmail.com>
2024-03-10 11:09:28 +0200

    House Form Model Created

---
+++ HouseRentingSystem.Web.ViewModels/House/HouseFormModel.cs
@@ -0,0 +1,42 @@
+﻿using HouseRentingSystem.Web.ViewModels.Category;
+using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem.Web.ViewModels.House
+{
+    using static Common.EntityValidationConstants.House;
+    public class HouseFormModel
+    {
+        //pri redirect categories mozhe da izgarmiat
+        public HouseFormModel()
+        {
+            this.Categories = new HashSet<HouseSelectCategoryFormModel>();
+        }
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        // mozem da slozim custom error message, oba4e vgradenia e dostato4no dobar,
+        //mozem da go promenim samo ako go iskame na bg
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [StringLength(ImageUrlMaxLength)]
+        [Display(Name = "Image Link")]
+        public string ImageUrl { get; set; } = null!;
+
+        [Range(typeof(decimal), PricePerMonthMinValue, PricePerMonthMaxValue)]
+        [Display(Name = "Monthly Price")]
+        public decimal PricePerMonth { get; set; }
+
+        [Display(Name ="Category")]
+        public int CategoryId { get; set; }
+        public IEnumerable<HouseSelectCategoryFormModel> Categories { get; set; }
+    }
+}
