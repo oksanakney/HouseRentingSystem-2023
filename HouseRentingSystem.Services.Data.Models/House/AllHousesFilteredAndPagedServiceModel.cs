a718aa666a546000b12dfe45e30a37102105c0ca
oksanakney <oksanakney@gmail.com>
2024-03-10 20:14:20 +0200

    AllHousesFilteredAndPagedService model added

---
+++ HouseRentingSystem.Services.Data.Models/House/AllHousesFilteredAndPagedServiceModel.cs
@@ -0,0 +1,15 @@
+﻿using HouseRentingSystem.Web.ViewModels.House;
+
+namespace HouseRentingSystem.Services.Data.Models.House
+{
+    //Service Models have to see View Models
+    public class AllHousesFilteredAndPagedServiceModel
+    {
+        public AllHousesFilteredAndPagedServiceModel()
+        {
+            this.Houses = new HashSet<HouseAllViewModel>();
+        }
+        public int TotalHousesCount { get; set; }
+        public IEnumerable<HouseAllViewModel> Houses { get; set; }
+    }
+}
