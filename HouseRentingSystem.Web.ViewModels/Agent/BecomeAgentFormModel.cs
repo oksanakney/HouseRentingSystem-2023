df6d707c8417bb51c989f7d5b347cdbaf22021a8
oksanakney <oksanakney@gmail.com>
2024-03-10 11:11:47 +0200

    Become Agent form model modified

--- HouseRentingSystem.Web.ViewModels/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem.Web.ViewModels/Agent/BecomeAgentFormModel.cs
@@ -1,7 +1,16 @@
-﻿namespace HouseRentingSystem.Web.ViewModels.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+
+namespace HouseRentingSystem.Web.ViewModels.Agent
 {
+    using static Common.EntityValidationConstants.Agent;
     public class BecomeAgentFormModel
     {
+        [Required]
+        [StringLength(PhoneNumberMaxLength, MinimumLength = PhoneNumberMinLength)]
+        [Phone]
+        [Display(Name = "Phone")]
         public string PhoneNumber { get; set; } = null!;
     }
 }
